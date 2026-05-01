1f6061008e44f1f05132d9d521be5e06ddb33cbd
adamarsyadfaizin <adamkian09@gmail.com>
2026-05-01 16:19:10 +0700

    chore: merapikan struktur file di resources

--- routes/web.php
+++ routes/web.php
@@ -3,26 +3,26 @@
 use App\Http\Controllers\RoomController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => view('home'))->name('home');
+Route::get('/', fn () => view('pages.home'))->name('home');
 
 Route::get('/rooms', [RoomController::class, 'index'])->name('rooms.index');
 
 Route::get('/contact', function () {
-    return view('contact');
+    return view('pages.contact');
 })->name('contact');
 
 Route::get('/gallery', function () {
-    return view('gallery');
+    return view('pages.gallery');
 })->name('gallery');
 
 Route::get('/signup', function () {
-    return view('signup');
+    return view('pages.signup');
 })->name('signup');
 
 Route::get('/login', function () {
-    return view('login');
+    return view('pages.login');
 })->name('login');
 
 Route::get('/profile', function () {
-    return view('profile');
+    return view('pages.profile');
 })->name('profile');
