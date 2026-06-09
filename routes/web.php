3a2d9666af2e4368998b4cc209cba2739762c924
adamarsyadfaizin <adamkian09@gmail.com>
2026-06-10 00:31:40 +0700

    fix: routing

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,50 @@
 <?php
 
 use App\Http\Controllers\RoomController;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AboutController;
-
 use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
+use App\Models\Booking;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/about', [AboutController::class, 'index'])->name('about');
 
-Route::get('/rooms', [App\Http\Controllers\RoomController::class, 'index'])->name('rooms.index');
+Route::get('/rooms', [RoomController::class, 'index'])->name('rooms.index');
+Route::get('/rooms/{room:nomor_kamar}/booking', [RoomController::class, 'createBooking'])->name('rooms.booking.create');
+Route::post('/rooms/{room:nomor_kamar}/booking', [RoomController::class, 'storeBooking'])->name('rooms.booking.store');
+Route::post('/rooms/{room:nomor_kamar}/reviews', [RoomController::class, 'storeReview'])
+    ->middleware('auth')
+    ->name('rooms.reviews.store');
+Route::get('/rooms/{room:nomor_kamar}', [RoomController::class, 'show'])->name('rooms.show');
 
-// Rute sementara untuk detail kamar karena belum ada RoomController@show dan view-nya
-Route::get('/rooms/{room}', function ($room) {
-    return redirect()->route('rooms.index');
-})->name('rooms.show');
+Route::get('/contact', [ContactController::class, 'index'])
+    ->name('contact');
 
-Route::get('/contact', function () {
-    return view('pages.contact');
-})->name('contact');
+Route::post('/contact', [ContactController::class, 'store'])
+    ->name('contact.store');
 
+Route::post('/contact/feedback', [ContactController::class, 'storeFeedback'])
+    ->name('contact.feedback.store');
 
+Route::get('/signup', [AuthController::class, 'showRegister'])->name('signup');
 
-Route::get('/signup', function () {
-    return view('pages.signup');
-})->name('signup');
+Route::post('/signup', [AuthController::class, 'register'])->name('signup.store');
 
-Route::get('/login', function () {
-    return view('pages.login');
-})->name('login');
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+
+Route::post('/login', [AuthController::class, 'login'])->name('login.store');
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::get('/profile', function () {
-    return view('pages.profile');
-})->name('profile');
+    $bookings = Booking::query()
+        ->with('room')
+        ->where('user_id', auth()->id())
+        ->latest()
+        ->get();
+
+    return view('pages.profile', compact('bookings'));
+})->middleware('auth')->name('profile');
