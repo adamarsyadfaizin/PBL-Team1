ad56855d2df84aade7c73475ea2ecf0c9eab960f
Rafi Abyantara Pratama <rafiabyantarapratama@gmail.com>
2026-06-09 22:25:06 +0700

    add home blade php

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,19 +7,7 @@
 @endpush
 
 @section('content')
-    <x-home.hero 
-        :settings="$homeSettings"
-        :stats="$homeStats"
-        :search-rooms="$searchRooms"
-        :guest-profile="$guestProfile"
-    />
-    <x-home.about 
-        :settings="$homeSettings"
-        :recommended-room="$highlightRooms->first()"
-    />
-    <x-home.features 
-        :settings="$homeSettings"
-        :rooms="$highlightRooms"
-        :guest-profile="$guestProfile"
-    />
+    <x-home.hero :settings="$homeSettings" :stats="$homeStats" :search-rooms="$searchRooms" :guest-profile="$guestProfile" />
+    <x-home.about :settings="$homeSettings" :recommended-room="$highlightRooms->first()" />
+    <x-home.features :settings="$homeSettings" :rooms="$highlightRooms" :guest-profile="$guestProfile" />
 @endsection
