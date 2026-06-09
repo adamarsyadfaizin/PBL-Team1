a2379345edbde01e24b7d180d2856e25da02d8db
Evan Raditya <evanraditya0608@gmail.com>
2026-06-09 20:54:11 +0700

    feat: add RoomReview models

---
+++ app/Models/RoomReview.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class RoomReview extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $table = 'room_reviews';
+
+    protected $primaryKey = 'id';
+
+    public $incrementing = false;
+
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'room_id',
+        'user_id',
+        'rating',
+        'comment',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'rating' => 'integer',
+        ];
+    }
+
+    public function room(): BelongsTo
+    {
+        return $this->belongsTo(Room::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
